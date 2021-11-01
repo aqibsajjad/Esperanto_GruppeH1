ba88a54f44293f0d598b85c31d24004e71c74b20
AndreasKroneReichl <s205362@student.dtu.dk>
2021-11-01 14:26:28 +0100

    Added topbar in tomato color

--- app/src/main/java/com/example/esperanto_gruppeh1/MainActivity.kt
+++ app/src/main/java/com/example/esperanto_gruppeh1/MainActivity.kt
@@ -6,8 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.esperanto_gruppeh1.ui.theme.Esperanto_GruppeH1Theme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +25,21 @@ class MainActivity : ComponentActivity() {
                     Greeting("Android")
                 }
             }
+            TopBarPreview()
         }
     }
+}
+@Composable
+fun Topbar(){
+    TopAppBar(
+        title = { Text( text= stringResource(R.string.app_name), fontSize = 24.sp ) },
+        backgroundColor = colorResource(id = R.color.red_200),
+        contentColor = Color.White
+    )
+}
+@Composable
+fun TopBarPreview(){
+    Topbar()
 }
 
 @Composable

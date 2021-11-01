93cd8fbcc1bc252479ed9b6f823169aa5a976109
AndreasKroneReichl <s205362@student.dtu.dk>
2021-11-01 16:36:30 +0100

    fixed text for bottomnavbar - mainscreen with top and bottom combined

--- app/src/main/java/com/example/esperanto_gruppeh1/MainActivity.kt
+++ app/src/main/java/com/example/esperanto_gruppeh1/MainActivity.kt
@@ -3,16 +3,16 @@ package com.example.esperanto_gruppeh1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import androidx.navigation.compose.rememberNavController
+import com.example.esperanto_gruppeh1.navigationbar.NavigationItem
 import com.example.esperanto_gruppeh1.ui.theme.Esperanto_GruppeH1Theme
 
 class MainActivity : ComponentActivity() {
@@ -25,7 +25,7 @@ class MainActivity : ComponentActivity() {
                     Greeting("Android")
                 }
             }
-            TopBarPreview()
+            MainScreen()
         }
     }
 }
@@ -34,12 +34,56 @@ fun Topbar(){
     TopAppBar(
         title = { Text( text= stringResource(R.string.app_name), fontSize = 24.sp ) },
         backgroundColor = colorResource(id = R.color.red_200),
-        contentColor = Color.White
+        contentColor = colorResource(id = R.color.white)
     )
 }
 @Composable
 fun TopBarPreview(){
     Topbar()
+}
+@Composable
+fun BottomNavigationBar(){
+    val items = listOf(
+        NavigationItem.Home,
+        NavigationItem.Channels,
+        NavigationItem.Favorites,
+        NavigationItem.Search
+    )
+    BottomNavigation(
+        backgroundColor = colorResource(id = R.color.red_200),
+         contentColor= Color.White
+    ){
+        items.forEach { item ->
+            BottomNavigationItem(
+                icon = { Icon(painterResource(id = item.icon), contentDescription = item.title) },
+                label = { Text(text = item.title)},
+                selectedContentColor = colorResource(id= R.color.white),
+                //unselectedContentColor = colorResource(id = R.color.white),
+                alwaysShowLabel = true,
+                selected = false,
+                onClick = {
+                }
+            )
+        }
+    }
+}
+@Composable
+fun BottomNavigationBarPreview(){
+    BottomNavigationBar()
+}
+
+@Composable
+fun MainScreen(){
+    Scaffold (
+        topBar = { Topbar()},
+        bottomBar = { BottomNavigationBar()}
+    ){
+            }
+}
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreview(){
+    MainScreen()
 }
 
 @Composable

56851ac56b266a1a79d964b0b66bcbc5eaea0cf4
aqibsajjad <aqib-sajjad@hotmail.com>
2021-11-15 10:59:39 +0100

    Fjernet JetPack compose. Tilføjet fragment_player.xml og fragment_search.xml samt SearchFragment.kt og PlayerFragment.kt.
    Der yderligere tilføjet nav.graph. hhv. navigation component.

--- app/src/main/java/com/example/esperanto_gruppeh1/MainActivity.kt
+++ app/src/main/java/com/example/esperanto_gruppeh1/MainActivity.kt
@@ -1,100 +1,31 @@
 package com.example.esperanto_gruppeh1
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
-import androidx.navigation.compose.rememberNavController
-import com.example.esperanto_gruppeh1.navigationbar.NavigationItem
-import com.example.esperanto_gruppeh1.ui.theme.Esperanto_GruppeH1Theme
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.esperanto_gruppeh1.databinding.ActivityMainBinding
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            Esperanto_GruppeH1Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
-            MainScreen()
-        }
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+        navController = navHostFragment.navController
+
+        setupActionBarWithNavController(navController)
     }
-}
-@Composable
-fun Topbar(){
-    TopAppBar(
-        title = { Text( text= stringResource(R.string.app_name), fontSize = 20.sp ) },
-        backgroundColor = colorResource(id = R.color.red_200),
-        contentColor = colorResource(id = R.color.white)
-    )
-}
-@Composable
-fun TopBarPreview(){
-    Topbar()
-}
-@Composable
-fun BottomNavigationBar(){
-    val items = listOf(
-        NavigationItem.Home,
-        NavigationItem.Channels,
-        NavigationItem.Favorites,
-        NavigationItem.Search
-    )
-    BottomNavigation(
-        backgroundColor = colorResource(id = R.color.red_200),
-         contentColor= colorResource(id = R.color.white)
-    ){
-        items.forEach { item ->
-            BottomNavigationItem(
-                icon = { Icon(painterResource(id = item.icon), contentDescription = item.title) },
-                label = { Text(text = item.title)},
-                selectedContentColor = colorResource(id= R.color.white),
-                //unselectedContentColor = colorResource(id = R.color.white),
-                alwaysShowLabel = true,
-                selected = false,
-                onClick = {
-                }
-            )
-        }
-    }
-}
-@Composable
-fun BottomNavigationBarPreview(){
-    BottomNavigationBar()
-}
 
-@Composable
-fun MainScreen(){
-    Scaffold (
-        topBar = { Topbar()},
-        bottomBar = { BottomNavigationBar()}
-    ){
-            }
-}
-@Preview(showBackground = true)
-@Composable
-fun MainScreenPreview(){
-    MainScreen()
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    Esperanto_GruppeH1Theme {
-        Greeting("Android")
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
